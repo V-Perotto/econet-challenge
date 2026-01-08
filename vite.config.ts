7f210fe3efb5d158fa57096213b39e57b86d39ea
V-Perotto <50595651+V-Perotto@users.noreply.github.com>
2026-01-08 16:27:40 -0300

    Add config files

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import { version } from './package.json'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/',
+  plugins: [vue(), vueJsx()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/variables.scss" as *;`,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  define: {
+    VITE_APP_VERSION: JSON.stringify(version),
+  },
+})
