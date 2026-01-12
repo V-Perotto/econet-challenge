cf85c09bfd455dc4859bf887ef5e7b261e5e9459
V-Perotto <50595651+V-Perotto@users.noreply.github.com>
2026-01-12 14:52:23 -0300

    Add directive Maska and update Toast

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,20 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import 'vue-toastification/dist/index.css'
+import { vMaska } from "maska/vue"
 import { createApp } from 'vue'
 import Toast, { POSITION } from 'vue-toastification'
 import App from '@/App.vue'
-
 import router from '@/routers/router'
-
-import 'bootstrap/dist/css/bootstrap.css'
-import 'vue-toastification/dist/index.css'
 
 const app = createApp(App)
 
+app.directive("maska", vMaska)
 app.provide('router', router)
 app.use(router)
 app.use(Toast, {
   position: POSITION.TOP_CENTER,
-  timeout: 5000,
+  timeout: 2000,
+  closeOnClick: true,
+  pauseOnHover: false  
 })
 app.mount('#app')
