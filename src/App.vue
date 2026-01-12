8669d4b7986f97aed92b65c1e323bf460bdbfaeb
V-Perotto <50595651+V-Perotto@users.noreply.github.com>
2026-01-12 14:51:58 -0300

    Fix RouterView

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
   <div id="app">
-    <router-view></router-view>
+    <RouterView ></RouterView>
   </div>
 </template>
